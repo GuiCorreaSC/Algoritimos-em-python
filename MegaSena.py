98fe48b8c9f96fe6d426e4167103ec4e95da164a
GuiCorreaSC <106286819+GuiCorreaSC@users.noreply.github.com>
2022-05-25 23:21:29 -0300

    Adicionado a lista
    sorteio = [] # cria uma lista para essa variavel

--- MegaSena.py
+++ MegaSena.py
@@ -1,6 +1,12 @@
 import random
-x = int(input("Quantas dezenas deseja apostar (De 6 a 15)? "))
-for a in range(0,x):
- r = random.randint(1,60)
- print(f'{r}--',end = '')
- 
+
+n = int(input("Quantas dezenas deseja sortear?"))
+print("Sorteei para você os seguintes números.")
+
+sorteio = [] # cria uma lista para essa variavel
+
+for i in range(0,n): # limita os numeros de inicio a fim
+  x = random.randint(1,60) # escolhe um numero random a cada fez que o range rodar
+  sorteio.append(x) # adiciona o numero sorteado na variavel 
+
+print(sorted(sorteio)) # sorted ordena os numeros do menor pro maior
